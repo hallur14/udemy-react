750f4dd05fa44f89938b44298e0cca680d475a17
Hallur <hallurolafs@gmail.com>
2018-10-12 16:08:00 +0000

    at lecture 109

--- react-course/expensify-app/src/app.js
+++ react-course/expensify-app/src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
@@ -10,13 +11,18 @@ import 'normalize.css/normalize.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill'}));
-store.dispatch(addExpense({ description: 'Gas bill'}));
-store.dispatch(setTextFilter('water'));
+store.dispatch(addExpense({ description: 'Water bill', amount: 5000}));
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000}));
+store.dispatch(addExpense({ description: 'Rent', amount: 109054}));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+ReactDOM.render(jsx, document.getElementById('app'));
